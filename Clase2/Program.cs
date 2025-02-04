67b607f9026b1939e215383d2fd288a5bda0257a
rposadas15 <ronaldoposadas0o@gmail.com>
2025-02-03 19:31:02 -0600

    Clase 2

---
+++ Clase2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using Antlr4.Runtime;
+
+class Program {
+    static void Main(string[] args) {
+        List<ErroresDTO> listaErrores = new List<ErroresDTO>();
+
+        var entrada = "fmt.Println(\"Esto tiene que ir en la consola\")";
+        var entradaParseada = new AntlrInputStream(entrada);
+
+        //Analisis lexico
+        var analisisLexico = new AnalizadorLexicoLexer(entradaParseada);
+        
+        //Analisis sintactico
+        var listaTokens = new CommonTokenStream(analisisLexico);
+        var analisiSintactico = new AnalizadorLexicoParser(listaTokens);
+        
+        var arbol = analisiSintactico.inicio();
+        Visitor visitor = new Visitor();
+        visitor.Visit(arbol);
+        Console.WriteLine("-----------------------------------------");
+        foreach (var consola in visitor.listaSalida) {
+            Console.WriteLine(consola);
+        }
+        Console.WriteLine("-----------------------------------------");
+    }
+}
